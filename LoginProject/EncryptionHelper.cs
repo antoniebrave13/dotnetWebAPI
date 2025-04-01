38a99b225bac46f5944e991d4d0aae1756467ffc
antonio.bravo <13165318+MrSimplevia2018@users.noreply.github.com>
2025-04-01 12:10:00 +0800

    dotnet web api
    dotnet web api

---
+++ LoginProject/EncryptionHelper.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace LoginProject.Helpers
+{
+    public static class EncryptionHelper
+    {
+        public static string EncryptString(string plainText)
+        {
+            // Example encryption logic (use a proper encryption method in production)
+            byte[] data = Encoding.UTF8.GetBytes(plainText);
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                byte[] hash = sha256.ComputeHash(data);
+                return Convert.ToBase64String(hash);
+            }
+        }
+    }
+}
+using LoginProject.Helpers;
+
+namespace LoginProject.Controllers
+{
+    // Existing code...
+}
---
+++ LoginProject/LoginProject/Controllers/LoginController.cs
@@ -0,0 +1,53 @@
+﻿using LoginProject.Helpers;
+using LoginProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace LoginProject.Controllers
+{
+    [RoutePrefix("api/Users")]
+    public class LoginController : ApiController
+    {
+        SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["conn"].ConnectionString);
+        SqlDataAdapter dataAdapter = null;
+
+        [HttpPost]
+        [Route("Login")]
+        public string Login(Employee employee)
+        {
+            string msg = string.Empty;
+            try
+            {
+                string hashedPassword = EncryptionHelper.EncryptString(employee.Password);
+                dataAdapter = new SqlDataAdapter("usp_Login", conn);
+                dataAdapter.SelectCommand.CommandType = CommandType.StoredProcedure;
+                dataAdapter.SelectCommand.Parameters.AddWithValue("@username", employee.Username);
+                dataAdapter.SelectCommand.Parameters.AddWithValue("@password", hashedPassword);
+                DataTable dt = new DataTable();
+                dataAdapter.Fill(dt);
+
+                if (dt.Rows.Count > 0)
+                {
+                    msg = "User is valid";
+                }
+                else
+                {
+                    msg = "User is invalid";
+                }
+            }
+            catch (Exception ex)
+            {
+                msg = ex.Message;
+            }
+
+            return msg;
+        }
+    }
+}
